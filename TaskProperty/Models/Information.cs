32db5b16c0a4728c557bba122d422be4a7db4ee5
baderalqallaf <baderalqallaf@BADERALQALLE446>
2024-12-25 09:07:03 +0000

    Add project files.

---
+++ TaskProperty/Models/Information.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProperty.Models
+{
+    public class Information
+    {
+        public string UserName;
+        private int Salariy;
+        List<Information> usernamelist = new List<Information>();
+
+        public void AddUsername(string username)
+        {
+            if (username == null)
+            {
+                Console.WriteLine("Please Enter username :");
+                username = Console.ReadLine();
+            }
+            else
+            {
+                usernamelist.Add(new Information { UserName = username });
+            }
+        }
+
+        public void GetUsername()
+        {
+            foreach (var item in usernamelist)
+            {
+                Console.WriteLine(item.UserName);
+            }
+        }
+
+        public int Salary { get { return Salariy; } set {
+                if (value >= 350 && value <= 900)
+                {
+                    Salariy = value;                }
+                else
+                {
+                    Console.WriteLine("Salary out of range");
+                }
+            }  }
+    }
+}
---
+++ TaskProperty/Program.cs
@@ -0,0 +1,19 @@
+﻿using TaskProperty.Models;
+
+Information info = new Information();
+
+Console.WriteLine("How many users do you want to enter ");
+int count = Convert.ToInt32((Console.ReadLine()));
+for (int i = 1; i <= count; i++)
+{
+    Console.WriteLine("enter username : " + count);
+    string username = Console.ReadLine();
+    info.AddUsername(username);
+}
+info.GetUsername();
+
+
+//info.Salary(salary);
+
+Console.WriteLine("Please enter Salary = ");
+info.Salary = Convert.ToInt32((Console.ReadLine()));
